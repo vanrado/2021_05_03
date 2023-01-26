44fefb3b7ce7bcc5867ece3ccd215acb982ee8c6
Radovan Racak <radovan.racak@gmail.com>
2023-01-26 10:10:35 +0100

    feat: check-in & report-loss features

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
-import { CheckinComponent } from './checkin.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LuggageDomainModule} from '@flight-workspace/luggage/domain';
+import {CheckinComponent} from './checkin.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [
+    CommonModule,
+    LuggageDomainModule,
+    // Add this:
+    RouterModule.forChild([
+      {path: '', component: CheckinComponent}
+    ])
+  ],
   declarations: [CheckinComponent],
   exports: [CheckinComponent],
 })
-export class LuggageFeatureCheckinModule {}
+export class LuggageFeatureCheckinModule {
+}
--- libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
-import { ReportLossComponent } from './report-loss.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LuggageDomainModule} from '@flight-workspace/luggage/domain';
+import {ReportLossComponent} from './report-loss.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [
+    CommonModule, LuggageDomainModule,
+    RouterModule.forChild([
+      {path: '', component: ReportLossComponent}
+    ])
+  ],
   declarations: [ReportLossComponent],
   exports: [ReportLossComponent],
 })
-export class LuggageFeatureReportLossModule {}
+export class LuggageFeatureReportLossModule {
+}
